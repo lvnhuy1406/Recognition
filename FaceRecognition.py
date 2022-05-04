74922572f1fbbc42c65c8b9d96d283b3370579fb
Lê Vũ Ngọc Huy <lvnhuy.19it2@vku.udn.vn>
2022-05-04 21:53:47 +0700

    Update

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -33,12 +33,12 @@ print(len(encodeListKnow))
 
 def joinNow(name):
     with open("JoinNow.csv", "r+") as file:
-        myDataList = file.readline()
+        myDataList = file.readlines()
         nameList = []
 
         for line in myDataList:
             entry = line.split(",")
-            nameList.append(entry)
+            nameList.append(entry[0])
 
         if name not in nameList:
             now = datetime.now()
@@ -49,28 +49,33 @@ capture = cv2.VideoCapture(0)
 
 while True:
     ret, frame = capture.read()
-    frameSize = cv2.resize(frame, (0, 0), None, fx=0.5, fy=0.5)
+    frameSize = cv2.resize(frame, (0, 0), None, fx=0.25, fy=0.25)
     frameSize = cv2.cvtColor(frameSize, cv2.COLOR_BGR2RGB)
 
     faceCurFrame = face_recognition.face_locations(frameSize)
-    encodeCurFrame = face_recognition.face_encodings(frameSize)
+    encodeCurFrame = face_recognition.face_encodings(frameSize, faceCurFrame)
 
     for encodeFace, faceLocat in zip(encodeCurFrame, faceCurFrame):
-        matchs = face_recognition.compare_faces(encodeListKnow, encodeFace)
+        matches = face_recognition.compare_faces(encodeListKnow, encodeFace)
         faceDis = face_recognition.face_distance(encodeListKnow, encodeFace)
-        print(faceDis)
+        # print(faceDis)
         matchIndex = np.argmin(faceDis)
 
-        if faceDis[matchIndex] < 0.50:
+        if matches[matchIndex]:
             name = classNames[matchIndex]
             joinNow(name)
+
         else:
             name = 'Unknown'
 
         y1, x2, y2, x1 = faceLocat
-        y1, x2, y2, x1 = y1*2, x2*2, y2*2, x1*2
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.putText(frame, name, (x2, y2), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
+        cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
+
+
+
 
     cv2.imshow('Face Recognition', frame)
     if cv2.waitKey(1) == ord("q"):
